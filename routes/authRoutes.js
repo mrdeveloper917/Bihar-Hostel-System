b8385ebdcdc0feb8b86d3571f707f7f39ddb16db
mrdeveloper917 <thakurayush995537@gmail.com>
2025-11-16 00:55:54 +0530

    ..

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,34 @@
+import express from "express";
+import { body } from "express-validator";
+import {
+  getLogin,
+  getRegister,
+  postRegister,
+  postLogin,
+  logout,
+} from "../controllers/authController.js";
 
-import express from 'express';
-import { body } from 'express-validator';
-import { getLogin, getRegister, postRegister, postLogin, logout } from '../controllers/authController.js';
 const router = express.Router();
 
-router.get('/login', getLogin);
-router.get('/register', getRegister);
-router.post('/register',
-  // body('email').isEmail().withMessage('Valid email required'),
-  // body('password').isLength({ min:6 }).withMessage('Password min 6 chars'),
-  postRegister);
-router.post('/login', postLogin);
-router.post('/logout', logout);
+router.get("/login", getLogin);
+router.get("/register", getRegister);
+
+// Register Validation
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Valid email required"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be minimum 6 characters"),
+  ],
+  postRegister
+);
+
+// Login
+router.post("/login", postLogin);
+
+// Logout
+router.post("/logout", logout);
+
 export default router;
